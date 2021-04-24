a22cea4b6fd4e6ca2db07d6911bf9a26d4c93eaf
sifat hassan <sifat007.sh7@gmail.com>
2021-04-24 20:14:01 +0600

    forget password done:Navigator.push(

--- lib/NetworkHandler.dart
+++ lib/NetworkHandler.dart
@@ -117,7 +117,20 @@ class NetworkHandler {
   }
 
 
-
+  Future<http.Response> patch(String url, Map<String, String> body) async {
+    String token = await storage.read(key: "token");
+    url = formater(url);
+    log.d(body);
+    var response = await http.patch(
+      url,
+      headers: {
+        "Content-type": "application/json",
+        "Authorization": "Bearer $token"
+      },
+      body: json.encode(body),
+    );
+    return response;
+  }
 
 
   String formater(String url){
